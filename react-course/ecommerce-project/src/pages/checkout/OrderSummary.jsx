75f237bdab52a0682e52039f01165c78861f3ba7
SyedSaif07 <syed.sky07@gmail.com>
2026-03-19 23:19:27 +0300

    delete cart item

--- react-course/ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ react-course/ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import { formatMoney } from "../../utils/money";
 import { DeliveryOptions } from "./DeliveryOptions";
+import axios from "axios";
 
 export function OrderSummary({ deliveryOptions, cart, getCartItems }) {
   return (
@@ -12,6 +13,10 @@ export function OrderSummary({ deliveryOptions, cart, getCartItems }) {
               return deliveryOption.id === cartItem.deliveryOptionId;
             },
           );
+          const deleteCartItem = async ()=>{
+            await axios.delete(`/api/cart-items/${cartItem.productId}`);
+            await getCartItems();
+          };
           return (
             <div key={cartItem.productId} className="cart-item-container">
               <div className="delivery-date">
@@ -39,7 +44,7 @@ export function OrderSummary({ deliveryOptions, cart, getCartItems }) {
                     <span className="update-quantity-link link-primary">
                       Update
                     </span>
-                    <span className="delete-quantity-link link-primary">
+                    <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>
                       Delete
                     </span>
                   </div>
